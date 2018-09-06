2848bbe75e1a7ae8137072a46ff102daaddaf90f
rafael <rafaelsm89@gmail.com>
2018-09-05 22:18:36 -0300

    fix

--- server/src/main/java/com/example/Greeter.java
+++ server/src/main/java/com/example/Greeter.java
@@ -12,8 +12,11 @@ public class Greeter {
 
   }
 
-  //TODO: Add javadoc comment
-  public String greet(String someone) {
+  /**
+   * My method
+   */
+  public final String greet(String someone) {
     return String.format("Hello, %s!", someone);
   }
 }
+
